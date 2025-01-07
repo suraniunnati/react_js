9816dc48ef2296e3f8dc366f08314420de9ad4a8
suraniunnati <suraniunnati5@gmail.com>
2025-01-07 21:20:42 +0530

    counter with redux

--- day22/src/redux/Store.js
+++ day22/src/redux/Store.js
@@ -1,11 +1,16 @@
-import { legacy_createStore } from 'redux';
+import { combineReducers, legacy_createStore } from 'redux';
 import { reducer1, reducer2} from './Reducer';
 
 
 
-export const reducer = (state = {}, action) => ({
-  counter1: reducer1(state.counter1, action),
-  counter2: reducer2(state.counter2, action),
-});
+// export const reducer = (state = {}, action) => ({
+//   counter1: reducer1(state.counter1, action),
+//   counter2: reducer2(state.counter2, action),
+// });
+
+export const reducer = combineReducers({
+  counter1: reducer1,
+  counter2: reducer2
+})
 
 export const Store = legacy_createStore(reducer);
